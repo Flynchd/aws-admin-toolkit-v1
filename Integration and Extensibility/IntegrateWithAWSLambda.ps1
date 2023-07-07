4c277b32840a7576cc7de0781c63cf7b2fc3323b
Flynchd <138042897+Flynchd@users.noreply.github.com>
2023-07-07 16:47:37 +0800

    Update IntegrateWithAWSLambda.ps1

--- Integration and Extensibility/IntegrateWithAWSLambda.ps1
+++ Integration and Extensibility/IntegrateWithAWSLambda.ps1
@@ -1,1 +1,18 @@
+# CustomAPIIntegration.ps1
 
+# Script to interact with AWS APIs or integrate with third-party services using PowerShell modules or REST API calls
+
+# Accept user input for the API endpoint URL
+$endpointUrl = Read-Host "Enter the API endpoint URL"
+
+# Accept user input for the HTTP method (e.g., GET, POST)
+$method = Read-Host "Enter the HTTP method"
+
+# Accept user input for the request body (optional)
+$payload = Read-Host "Enter the request body (optional)"
+
+# AWS CLI command or PowerShell code to make the API call
+$apiCallCommand = Read-Host "Enter the API call command"
+
+# Execute the API call command
+Invoke-Expression $apiCallCommand
