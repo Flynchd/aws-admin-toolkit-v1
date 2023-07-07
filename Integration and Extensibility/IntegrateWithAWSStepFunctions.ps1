d76c74c92b06bfb014554440e96395711e626595
Flynchd <138042897+Flynchd@users.noreply.github.com>
2023-07-07 16:47:29 +0800

    Update IntegrateWithAWSStepFunctions.ps1

--- Integration and Extensibility/IntegrateWithAWSStepFunctions.ps1
+++ Integration and Extensibility/IntegrateWithAWSStepFunctions.ps1
@@ -1,1 +1,12 @@
+# IntegrateWithAWSStepFunctions.ps1
 
+# Script to define and execute complex workflows using AWS Step Functions
+
+# Accept user input for the Step Functions state machine definition file path
+$definitionFilePath = Read-Host "Enter the path to the Step Functions state machine definition file"
+
+# AWS CLI command to create a Step Functions state machine
+$awsCliCommand = "aws stepfunctions create-state-machine --name <STATE_MACHINE_NAME> --definition file://$definitionFilePath --role-arn <STATE_MACHINE_ROLE_ARN>"
+
+# Execute the AWS CLI command
+Invoke-Expression $awsCliCommand
