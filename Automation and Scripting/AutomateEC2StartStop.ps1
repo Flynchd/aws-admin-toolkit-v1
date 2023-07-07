d3b13b140472cb26093643cc368f884b8fe4f9ac
Flynchd <138042897+Flynchd@users.noreply.github.com>
2023-07-07 16:42:34 +0800

    Update AutomateEC2StartStop.ps1

--- Automation and Scripting/AutomateEC2StartStop.ps1
+++ Automation and Scripting/AutomateEC2StartStop.ps1
@@ -1,1 +1,15 @@
+# AutomateEBSSnapshotCreation.ps1
 
+# Script to automate the creation of EBS snapshots for backup purposes
+
+# Accept user input for the volume ID
+$volumeId = Read-Host "Enter the EBS volume ID"
+
+# Accept user input for the snapshot description
+$description = Read-Host "Enter the snapshot description"
+
+# AWS CLI command to create an EBS snapshot
+$awsCliCommand = "aws ec2 create-snapshot --volume-id $volumeId --description $description"
+
+# Execute the AWS CLI command
+Invoke-Expression $awsCliCommand
