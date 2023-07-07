6a8442b3a0c1a7e4ffca4bd2b40e845684868ab3
Flynchd <138042897+Flynchd@users.noreply.github.com>
2023-07-07 16:39:01 +0800

    Update CreateIAMUser.ps1

--- Resource Management/CreateIAMUser.ps1
+++ Resource Management/CreateIAMUser.ps1
@@ -1,1 +1,19 @@
 
+# CreateIAMUser.ps1
+
+# Script to create an IAM user with specific permissions and access keys
+
+# AWS CLI command to create an IAM user
+$awsCliCommand = "aws iam create-user --user-name <USER_NAME>"
+
+# Execute the AWS CLI command
+Invoke-Expression $awsCliCommand
+
+# AWS CLI command to create access keys for the IAM user
+$awsCliCommand = "aws iam create-access-key --user-name <USER_NAME>"
+
+# Execute the AWS CLI command
+$accessKeyOutput = Invoke-Expression $awsCliCommand
+
+# Display the access key details
+$accessKeyOutput | ConvertFrom-Json | Select-Object -ExpandProperty AccessKey
