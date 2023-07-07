a9698a82eac4c66e631cbf94818be16f7ea1b762
Flynchd <138042897+Flynchd@users.noreply.github.com>
2023-07-07 16:38:47 +0800

    Update CreateS3Bucket.ps1

--- Resource Management/CreateS3Bucket.ps1
+++ Resource Management/CreateS3Bucket.ps1
@@ -1,1 +1,10 @@
+# CreateS3Bucket.ps1
+
+# Script to create an S3 bucket with specified configurations
+
+# AWS CLI command to create an S3 bucket
+$awsCliCommand = "aws s3api create-bucket --bucket <BUCKET_NAME> --region <REGION> --create-bucket-configuration LocationConstraint=<REGION>"
+
+# Execute the AWS CLI command
+Invoke-Expression $awsCliCommand
 
