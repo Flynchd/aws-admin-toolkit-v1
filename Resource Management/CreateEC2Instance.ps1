559542e4b39839e500971a9bbfebaafbcd48bcec
Flynchd <138042897+Flynchd@users.noreply.github.com>
2023-07-07 16:39:15 +0800

    Update CreateEC2Instance.ps1

--- Resource Management/CreateEC2Instance.ps1
+++ Resource Management/CreateEC2Instance.ps1
@@ -1,1 +1,9 @@
+# CreateEC2Instance.ps1
 
+# Script to provision EC2 instances with specified parameters
+
+# AWS CLI command to create an EC2 instance
+$awsCliCommand = "aws ec2 run-instances --image-id <IMAGE_ID> --instance-type <INSTANCE_TYPE> --key-name <KEY_PAIR_NAME> --security-group-ids <SECURITY_GROUP_ID> --subnet-id <SUBNET_ID>"
+
+# Execute the AWS CLI command
+Invoke-Expression $awsCliCommand
