4ff9103f82aad85fa3d9ff438c05cf4260d01630
Flynchd <138042897+Flynchd@users.noreply.github.com>
2023-07-07 16:42:25 +0800

    Update ScriptAWSCLICommands.ps1

--- Automation and Scripting/ScriptAWSCLICommands.ps1
+++ Automation and Scripting/ScriptAWSCLICommands.ps1
@@ -1,1 +1,9 @@
+# ScriptAWSCLICommands.ps1
 
+# Script to execute custom AWS CLI commands
+
+# Accept user input for the AWS CLI command
+$awsCliCommand = Read-Host "Enter the AWS CLI command"
+
+# Execute the AWS CLI command
+Invoke-Expression $awsCliCommand
