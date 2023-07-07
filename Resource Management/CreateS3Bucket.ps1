e8dbbf12ddf6d8db86269bd14ba49fc7e159a043
Flynchd <138042897+Flynchd@users.noreply.github.com>
2023-07-07 17:58:45 +0800

    Update CreateS3Bucket.ps1

--- Resource Management/CreateS3Bucket.ps1
+++ Resource Management/CreateS3Bucket.ps1
@@ -1,10 +1,24 @@
 # CreateS3Bucket.ps1
 
-# Script to create an S3 bucket with specified configurations
+# Script to create an S3 bucket
 
-# AWS CLI command to create an S3 bucket
-$awsCliCommand = "aws s3api create-bucket --bucket <BUCKET_NAME> --region <REGION> --create-bucket-configuration LocationConstraint=<REGION>"
+$isValid = $false
+
+do {
+    # Prompt the user for the bucket name
+    $bucketName = Read-Host "Enter the bucket name"
+
+    # Validate the input
+    if (-not [string]::IsNullOrWhiteSpace($bucketName)) {
+        $isValid = $true
+    }
+    else {
+        Write-Host "Invalid input. Please provide a valid bucket name."
+    }
+} while (-not $isValid)
+
+# Create the S3 bucket using AWS CLI command
+$awsCliCommand = "aws s3api create-bucket --bucket $bucketName"
 
 # Execute the AWS CLI command
 Invoke-Expression $awsCliCommand
-
