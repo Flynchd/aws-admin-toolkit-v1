6036cbb8e1130543d5f80f4c0938fe79854883ec
Flynchd <138042897+Flynchd@users.noreply.github.com>
2023-07-07 16:40:57 +0800

    Update EnableCloudWatchLogs.ps1

--- Monitoring and Logging/EnableCloudWatchLogs.ps1
+++ Monitoring and Logging/EnableCloudWatchLogs.ps1
@@ -1,1 +1,16 @@
 
+# EnableCloudWatchLogs.ps1
+
+# Script to enable CloudWatch Logs for EC2 instances or other AWS services
+
+# Accept user input for the instance ID
+$instanceId = Read-Host "Enter the EC2 instance ID"
+
+# Accept user input for the log group name
+$logGroupName = Read-Host "Enter the log group name"
+
+# AWS CLI command to enable CloudWatch Logs for the instance
+$awsCliCommand = "aws logs create-log-stream --log-group-name $logGroupName --log-stream-name $instanceId"
+
+# Execute the AWS CLI command
+Invoke-Expression $awsCliCommand
