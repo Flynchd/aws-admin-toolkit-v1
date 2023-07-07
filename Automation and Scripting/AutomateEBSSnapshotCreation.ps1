2fd0c316e77e5b1c017acfeca191e5dbeaa96c2f
Flynchd <138042897+Flynchd@users.noreply.github.com>
2023-07-07 16:42:40 +0800

    Update AutomateEBSSnapshotCreation.ps1

--- Automation and Scripting/AutomateEBSSnapshotCreation.ps1
+++ Automation and Scripting/AutomateEBSSnapshotCreation.ps1
@@ -1,1 +1,15 @@
+# AutomateEC2StartStop.ps1
 
+# Script to automatically start or stop EC2 instances based on a predefined schedule
+
+# Accept user input for the instance ID
+$instanceId = Read-Host "Enter the EC2 instance ID"
+
+# Accept user input for the desired action (start or stop)
+$action = Read-Host "Enter the action to perform (start/stop)"
+
+# AWS CLI command to start or stop the EC2 instance
+$awsCliCommand = "aws ec2 $action-instances --instance-ids $instanceId"
+
+# Execute the AWS CLI command
+Invoke-Expression $awsCliCommand
